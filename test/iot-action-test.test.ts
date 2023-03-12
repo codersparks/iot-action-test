37c37da4b87936aee47b7fb32bd1dbcf4799ea88
coder sparks <coder@DESKTOP-4JS077R.localdomain>
2023-03-12 11:05:19 +0000

    Initial commit

---
+++ test/iot-action-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IotActionTest from '../lib/iot-action-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iot-action-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IotActionTest.IotActionTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
